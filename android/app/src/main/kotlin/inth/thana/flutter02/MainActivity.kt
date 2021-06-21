7122c1357db12742df0f8a94a3c9972c68c77010
thana19 <thana.j@gmail.com>
2021-06-21 12:25:35 +0700

    first commit

---
+++ android/app/src/main/kotlin/inth/thana/flutter02/MainActivity.kt
@@ -0,0 +1,6 @@
+package inth.thana.flutter02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home/HomeStack.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter02/pages/home/AboutPage.dart';
+import 'package:flutter02/pages/home/HomePage.dart';
+
+class HomeStack extends StatefulWidget {
+  const HomeStack({Key? key}) : super(key: key);
+
+  @override
+  _HomeStackState createState() => _HomeStackState();
+}
+
+class _HomeStackState extends State<HomeStack> {
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      initialRoute: 'homestack/home',
+      onGenerateRoute: (RouteSettings settings){
+        WidgetBuilder builder;
+        switch (settings.name) {
+          case 'homestack/home':
+            builder = (BuildContext context) => HomePage();
+            break;
+          case 'homestack/about':
+            builder = (BuildContext context) => AboutPage();
+            break;
+
+          default:
+            throw Exception('Invalid route: ${settings.name}');
+        }
+        return MaterialPageRoute<void>(builder: builder, settings: settings);
+      },
+    );
+  }
+}
